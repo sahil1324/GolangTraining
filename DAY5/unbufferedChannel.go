12c2898142be1222c68a975ebe8b4be1e4676267
sahil1324 <39096771+sahil1324@users.noreply.github.com>
2020-04-24 12:57:50 +0530

    Create unbufferedChannel.go

---
+++ DAY5/unbufferedChannel.go
@@ -0,0 +1,32 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func checkLink(ch chan string, link string){
+	_,err := http.Get(link)
+	if err!=nil{
+		//fmt.Println(link,":-  Might Be DOWN")
+		ch <- link+"  Might Be DOWN"
+	}else{
+	   //fmt.Println(link,":-   Is Up")
+	   ch <- link+"   Is Up"
+	}
+}
+func main(){
+	links := []string{
+		"https://www.google.com",
+		"https://www.yahoo.com",
+		"https://www.golang.org",
+		"https://www.gmail.com",
+	}
+	//unbuffered channel
+	ch := make(chan string)
+	for _,link :=  range links{
+	   go checkLink(ch,link)
+	}
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
